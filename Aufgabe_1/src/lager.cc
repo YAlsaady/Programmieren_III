64581341e4ab15407b1a19db13aefc015620e156
Oliver <oliverlucaschmidt@gmail.com>
2023-09-28 11:31:18 +0200

    Add getter und setter
    Setter ist noch nicht Fertig

--- Aufgabe_1/src/lager.cc
+++ Aufgabe_1/src/lager.cc
@@ -4,3 +4,34 @@
 Artikel::Artikel(string name, unsigned int num, unsigned int lagerbestand,
           unsigned int masseinheit, double verkaufpreis, double normpreis){
 }
+
+ 
+string Artikel getName(){
+    return this->artikelname;
+}
+string Artikel getNummer(){
+    return this->nummer;
+}
+unsigned int Artikel getLagerabstand(){
+    return this->lagerbestand;
+}
+string Artikel getMasseinheit(){
+    return this->{kg, liter, stueck};
+}
+double Artikel getVerkaufpreis(){
+    return this->verkaufpreis;
+}
+double Artikel getNormpreis(){
+    return this->normpreis;
+}
+
+void Artikel setName(string name){
+    this->artikelname = name;
+}
+void Artikel setNummer(string num){
+    this->
+}
+  void setLagerbestand(unsigned int bestand);
+  void setMasseinheit(masseinheit einheit);
+  void setVerkaufpreis(double vp);
+  void setNorpreis(double np);
