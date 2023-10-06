6af7d8faa3bc962be5ae1c1cb609f09ae13b94a9
Oliver <oliverlucaschmidt@gmail.com>
2023-10-06 17:40:50 +0200

    Doxygen wurde generiert

---
+++ Aufgabe_1/doc/html/search/all_4.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['getartikelnummer_0',['getArtikelnummer',['../classArtikel.html#a03226a80f8f65e33ef55370e524d85cf',1,'Artikel']]],
+  ['getgruppe_1',['getgruppe',['../classWarengruppen.html#ac8850d8bf5e312c620125495c2235b4c',1,'Warengruppen::getGruppe()'],['../classArtikel.html#ad63de1c4058b5b31bdc74f549e5d2dfa',1,'Artikel::getGruppe() const']]],
+  ['getlagerabstand_2',['getLagerabstand',['../classArtikel.html#a2d3babab7c98f37b16e4547e4c298306',1,'Artikel']]],
+  ['getlosgroesse_3',['getLosgroesse',['../classSchuettgut.html#a12400a8813b499fbef8fc4ea44f2e644',1,'Schuettgut']]],
+  ['getmasseinheit_4',['getMasseinheit',['../classArtikel.html#a60c48aa90d170388b041432dabaacbb5',1,'Artikel']]],
+  ['getname_5',['getName',['../classArtikel.html#a5d6957c608452333fbd97dfe68903538',1,'Artikel']]],
+  ['getnormpreis_6',['getNormpreis',['../classArtikel.html#a9fe9802d1739baca518a46994a7e331d',1,'Artikel']]],
+  ['getverkaufpreis_7',['getVerkaufpreis',['../classArtikel.html#abb39b12f98c3bf5804f47b673627c261',1,'Artikel']]],
+  ['getvolume_8',['getVolume',['../classFluessigkeit.html#ab5da321475ed03f532f5812f4d256539',1,'Fluessigkeit']]],
+  ['gruppe_9',['gruppe',['../classArtikel.html#acc0e9e2efdb39f46ccabf1ba845f5047',1,'Artikel']]]
+];
---
+++ Aufgabe_1/doc/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['addgruppe_0',['addGruppe',['../classWarengruppen.html#aa41ecb8faea7854d3f0208cc3070006f',1,'Warengruppen']]],
+  ['artikel_1',['Artikel',['../classArtikel.html#a18c5b0543f34f969a2869192bf8afc7c',1,'Artikel']]]
+];
