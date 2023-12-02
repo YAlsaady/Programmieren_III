c88d345b3fa6c26858b7fce03904ca7611147085
Oliver Schmidt <oliverlucaschmidt@gmail.com>
2023-12-02 17:15:40 +0100

    Fertig

--- Aufgabe_4_Olli/src/stats.cc
+++ Aufgabe_4_Olli/src/stats.cc
@@ -13,58 +13,59 @@
 using namespace std;
 using namespace std::placeholders;
 
+/**
+ * @brief Hauptfunktion des Programms.
+ *
+ * - Füllt einen Vektor mit Zufallszahlen.
+ * - Sortiert den Vektor aufsteigend und gibt ihn aus.
+ * - Ermittelt die Häufigkeit der Zufallszahlen und stellt sie als Balkengrafik
+ * dar.
+ * - Berechnet den Durchschnitt der Häufigkeiten.
+ *
+ * @return Rückgabewert 0 bei erfolgreicher Ausführung.
+ */
 int main() {
   vector<unsigned int> v;
 
-  /* 1. Füllen von v mit 3902365 Zufallswerten. */
+  // 1. Füllen von v mit 3902365 Zufallswerten.
   generate_n(back_inserter(v), 3902365, Data(1, 100));
-  cout << "Vektor voll" << endl;
 
-  /* 2. aufsteigend sortieren und mit ostream_iterator ausgeben. */
-  sort(v.begin(), v.end());
-
+  // 2. aufsteigend sortieren und mit ostream_iterator ausgeben.
   sort(v.begin(), v.end());
   copy(v.begin(), v.end(), ostream_iterator<unsigned int>(cout, ", "));
   cout << endl;
 
-  /* 3. Häufigkeit ermitteln. */
-  /* 4. Häufigkeit als Balkengrafik darstellen. */
+  // 3. Häufigkeit ermitteln.
+  map<unsigned int, unsigned int> frequencyMap;
+  for_each(v.begin(), v.end(),
+           [&frequencyMap](unsigned int val) { frequencyMap[val]++; });
 
-  map<unsigned int, unsigned int> frequency_map;
-  for (const auto &num : v) {
-    frequency_map[num]++;
-  }
-
-  cout << "Häufigkeit der Werte und Balkendiagramm:" << endl;
-  for (const auto &pair : frequency_map) {
-    double balken_ = pair.second / (double)1000;
-
-    if (pair.first < 10) {
-      cout << "00" << pair.first << ": " << pair.second << " ";
-    } else if (pair.first < 100) {
-      cout << "0" << pair.first << ": " << pair.second << " ";
-    } else {
-      cout << pair.first << ": " << pair.second << " ";
-    }
-
-    for (int i = 0; i <= balken_; i++) {
+  // 4. Häufigkeit als Balkengrafik darstellen.
+  for (const auto &entry : frequencyMap) {
+    cout << setw(3) << entry.first << ": " << setw(6) << entry.second << " ";
+    for (unsigned int i = 0; i < entry.second / 1000; ++i) {
       cout << "#";
     }
     cout << endl;
   }
 
-  /* 5. Durchschnitt berechnen und ausgeben. */
-  double average_frequency =
-      accumulate(begin(frequency_map), end(frequency_map), 0.0,
-                 [](double acc, const pair<unsigned int, unsigned int> &p) {
-                   return acc + p.second;
-                 }) /
-      frequency_map.size();
-
-  cout << "Durchschnitt der Häufigkeiten: " << average_frequency << endl;
+  // 5. Durchschnitt berechnen und ausgeben.
+  double average = accumulate(frequencyMap.begin(), frequencyMap.end(), 0.0,
+                              [](double sum, const auto &entry) {
+                                return sum + entry.second;
+                              }) /
+                   frequencyMap.size();
+  cout << "Durchschnittliche Häufigkeit: " << average << endl;
+  
   double average_value = accumulate(begin(v), end(v), 0.0) / v.size();
 
   cout << "Durchschnitt der Zufallszahlen: " << average_value << endl;
-}
 
-/* 6. Zusatzaufgabe nicht vergessen! */
+  /**
+   * @brief Zusatzaufgabe .
+   * Hier könnten weitere Funktionen oder Verbesserungen implementiert werden,
+   * um die Fähigkeiten des Programms zu erweitern.
+   */
+
+  return 0;
+}
--- Aufgabe_4_Olli/src/stats.h
+++ Aufgabe_4_Olli/src/stats.h
@@ -2,16 +2,28 @@
 #define STATS_H_
 #include <random>
 
+/**
+ * @brief Klasse für die Generierung von Zufallszahlen im angegebenen Intervall.
+ */
 class Data {
   std::mt19937 rng;
   std::uniform_int_distribution<unsigned int> dist;
-public:
-  Data(unsigned int a, unsigned int b)
-    : rng(std::random_device()()), dist(a,b) {}
 
-  auto operator()(void) {
-    return dist(rng);
-  }
+public:
+  /**
+   * @brief Konstruktor für die Initialisierung des Zufallszahlengenerators und
+   * der Verteilung.
+   * @param a Untere Grenze des Intervalls.
+   * @param b Obere Grenze des Intervalls.
+   */
+  Data(unsigned int a, unsigned int b)
+      : rng(std::random_device()()), dist(a, b) {}
+
+  /**
+   * @brief Operatorfunktion zum Generieren einer Zufallszahl.
+   * @return Zufallszahl im spezifizierten Intervall.
+   */
+  auto operator()(void) { return dist(rng); }
 };
 
 #endif /* STATS_H_ */
